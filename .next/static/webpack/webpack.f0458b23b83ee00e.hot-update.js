4230341504c4fec0c529bfbd3786d46a47607f52
SHAH JI <abhinavshah133@gmail.com>
2025-06-24 23:58:23 +0530

    bug fix

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "30b79bb931da6687"; }
+/******/ 	__webpack_require__.h = function() { return "115b14a49d2c712f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
